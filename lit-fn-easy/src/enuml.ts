d98f565313491cb4c83a7b697a73f6d04b44db89
AnNingUI <an3533581512@gmail.com>
2025-07-17 01:41:31 +0800

    refactor(lit-fn-easy): remove unused memoize functions
    - Removed the memoize function from the enuml.ts file.
    - This function is not used in the project, so it was removed to simplify the code base

--- lit-fn-easy/src/enuml.ts
+++ lit-fn-easy/src/enuml.ts
@@ -117,16 +117,3 @@ export const Result = {
 	Err: (message: string | LitNothing) =>
 		ResultBuilder("Err", { message }) as ResultCase<"Err", string>,
 };
-
-export function memoize<T extends (...args: any[]) => any>(fn: T): T {
-	const cache = new Map<string, ReturnType<T>>();
-	return ((...args: any[]) => {
-		const key = JSON.stringify(args);
-		if (cache.has(key)) {
-			return cache.get(key)!;
-		}
-		const result = fn(...args);
-		cache.set(key, result);
-		return result;
-	}) as T;
-}
